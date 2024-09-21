4a701e5b0bd1f6a73d6cb4b3c38432fa33604078
Rafael Ferrete <rafaelferrete333@gmail.com>
2024-09-21 15:15:15 -0300

    update

---
+++ cryptography.php
@@ -0,0 +1,21 @@
+<?php
+
+$usuario = 'etec'; 
+$senha = md5('123456'); 
+
+// Captura os dados do formulário
+$user_digi = $_POST['campo_usuario'];
+$pass_digi = md5($_POST['campo_senha']); // Criptografa a senha digitada
+
+// Verifica se o usuário e a senha estão corretos
+if ($usuario === $user_digi && $senha === $pass_digi) {
+    // Redireciona para a página de boas-vindas
+    header("Location: page.php");
+    exit();
+} else {
+    // Mensagem de erro caso o login falhe
+    header("Location: index.php");
+    exit();
+    echo "Usuário ou senha incorretos.";
+}
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <div>
+        <form action="cryptography.php" method="post">
+            <label for="usuario">Usuário</label><br>
+            <input type="text" name="campo_usuario" id="usuario"><br>
+
+            <label for="senha">Senha</label><br>
+            <input type="password" name="campo_senha" id="senha"><br>
+
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
+</body>
+</html>
